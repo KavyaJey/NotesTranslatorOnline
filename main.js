4e496a3cf61ba5b74dd606c20a4b60f8decd2f09
KavyaJey <34611404+KavyaJey@users.noreply.github.com>
2020-05-25 16:11:58 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -14,96 +14,98 @@ function saveAndChange() {
 
   if (translation == "toCarnatic") {
 
-	translated = original.replace(/C/g, 'S'); 
-	translated = translated.replace(new RegExp('C', 'g'), 'S');
 
-	translated = translated.replace(/C#/g, 'R1'); 
-	translated = translated.replace(new RegExp('C#', 'g'), 'R1');
 
-	translated = translated.replace(/Db/g, 'R1'); 
-	translated = translated.replace(new RegExp('Db', 'g'), 'R1');
+	translated = original.replace(/C#/gi, 'R1'); 
+	translated = translated.replace(new RegExp('C#', 'gi'), 'R1');
 
-	translated = translated.replace(/D/g, 'R2'); 
-	translated = translated.replace(new RegExp('D', 'g'), 'R2');
+	translated = translated.replace(/C/gi, 'S'); 
+	translated = translated.replace(new RegExp('C', 'gi'), 'S');
 
-	translated = translated.replace(/D#/g, 'G1'); 
-	translated = translated.replace(new RegExp('D#', 'g'), 'G1');
+	translated = translated.replace(/Db/gi, 'R1'); 
+	translated = translated.replace(new RegExp('Db', 'gi'), 'R1');
 
-	translated = translated.replace(/Eb/g, 'G1'); 
-	translated = translated.replace(new RegExp('Eb', 'g'), 'G1');
+	translated = translated.replace(/D#/gi, 'G1'); 
+	translated = translated.replace(new RegExp('D#', 'gi'), 'G1');
 
-	translated = translated.replace(/E/g, 'G2'); 
-	translated = translated.replace(new RegExp('E', 'g'), 'G2');
+	translated = translated.replace(/D/gi, 'R2'); 
+	translated = translated.replace(new RegExp('D', 'gi'), 'R2');
 
-	translated = translated.replace(/F/g, 'M1'); 
-	translated = translated.replace(new RegExp('F', 'g'), 'M1');
+	translated = translated.replace(/Eb/gi, 'G1'); 
+	translated = translated.replace(new RegExp('Eb', 'gi'), 'G1');
 
-	translated = translated.replace(/F#/g, 'M2'); 
-	translated = translated.replace(new RegExp('F#', 'g'), 'M2');
+	translated = translated.replace(/E/gi, 'G2'); 
+	translated = translated.replace(new RegExp('E', 'gi'), 'G2');
 
-	translated = translated.replace(/Gb/g, 'M2'); 
-	translated = translated.replace(new RegExp('Gb', 'g'), 'M2');
+	translated = translated.replace(/F#/gi, 'M2'); 
+	translated = translated.replace(new RegExp('F#', 'gi'), 'M2');
 
-	translated = translated.replace(/G/g, 'P'); 
-	translated = translated.replace(new RegExp('G', 'g'), 'P');
+	translated = translated.replace(/F/gi, 'M1'); 
+	translated = translated.replace(new RegExp('F', 'gi'), 'M1');
 
-	translated = translated.replace(/G#/g, 'D1'); 
-	translated = translated.replace(new RegExp('G#', 'g'), 'D1');
+	translated = translated.replace(/Gb/gi, 'M2'); 
+	translated = translated.replace(new RegExp('Gb', 'gi'), 'M2');
 
-	translated = translated.replace(/Ab/g, 'D1'); 
-	translated = translated.replace(new RegExp('Ab', 'g'), 'D1');
+	translated = translated.replace(/G#/gi, 'D1'); 
+	translated = translated.replace(new RegExp('G#', 'gi'), 'D1');
 
-	translated = translated.replace(/A/g, 'D2'); 
-	translated = translated.replace(new RegExp('A', 'g'), 'D2');
+	translated = translated.replace(/G/gi, 'P'); 
+	translated = translated.replace(new RegExp('G', 'gi'), 'P');
 
-	translated = translated.replace(/A#/g, 'N1'); 
-	translated = translated.replace(new RegExp('A#', 'g'), 'N1');
+	translated = translated.replace(/Ab/gi, 'D1'); 
+	translated = translated.replace(new RegExp('Ab', 'gi'), 'D1');
 
-	translated = translated.replace(/Bb/g, 'N1'); 
-	translated = translated.replace(new RegExp('Bb', 'g'), 'N1');
+	translated = translated.replace(/A#/gi, 'N1'); 
+	translated = translated.replace(new RegExp('A#', 'gi'), 'N1');
 
-	translated = translated.replace(/B/g, 'N2'); 
-	translated = translated.replace(new RegExp('B', 'g'), 'N2');
+	translated = translated.replace(/A/gi, 'D2'); 
+	translated = translated.replace(new RegExp('A', 'gi'), 'D2');
+
+	translated = translated.replace(/Bb/gi, 'N1'); 
+	translated = translated.replace(new RegExp('Bb', 'gi'), 'N1');
+
+	translated = translated.replace(/B/gi, 'N2'); 
+	translated = translated.replace(new RegExp('B', 'gi'), 'N2');
 
   }
 
   if (translation == "toWestern") {
 
-	translated = original.replace(/S/g, 'C'); 
-	translated = translated.replace(new RegExp('S', 'g'), 'C');
+	translated = original.replace(/S/gi, 'C'); 
+	translated = translated.replace(new RegExp('S', 'gi'), 'C');
 
-	translated = translated.replace(/R1/g, 'C#'); 
-	translated = translated.replace(new RegExp('R1', 'g'), 'C#');
+	translated = translated.replace(/R1/gi, 'C#'); 
+	translated = translated.replace(new RegExp('R1', 'gi'), 'C#');
 
-	translated = translated.replace(/R2/g, 'D'); 
-	translated = translated.replace(new RegExp('R2', 'g'), 'D');
+	translated = translated.replace(/R2/gi, 'D'); 
+	translated = translated.replace(new RegExp('R2', 'gi'), 'D');
 
-	translated = translated.replace(/G1/g, 'Eb'); 
-	translated = translated.replace(new RegExp('G1', 'g'), 'Eb');
+	translated = translated.replace(/G1/gi, 'Eb'); 
+	translated = translated.replace(new RegExp('G1', 'gi'), 'Eb');
 
-	translated = translated.replace(/G2/g, 'E'); 
-	translated = translated.replace(new RegExp('G2', 'g'), 'E');
+	translated = translated.replace(/G2/gi, 'E'); 
+	translated = translated.replace(new RegExp('G2', 'gi'), 'E');
 
-	translated = translated.replace(/M1/g, 'F'); 
-	translated = translated.replace(new RegExp('M1', 'g'), 'F');
+	translated = translated.replace(/M1/gi, 'F'); 
+	translated = translated.replace(new RegExp('M1', 'gi'), 'F');
 
-	translated = translated.replace(/M2/g, 'F#'); 
-	translated = translated.replace(new RegExp('M2', 'g'), 'F#');
+	translated = translated.replace(/M2/gi, 'F#'); 
+	translated = translated.replace(new RegExp('M2', 'gi'), 'F#');
 
-	translated = translated.replace(/P/g, 'G'); 
-	translated = translated.replace(new RegExp('P', 'g'), 'G');
+	translated = translated.replace(/P/gi, 'G'); 
+	translated = translated.replace(new RegExp('P', 'gi'), 'G');
 
-	translated = translated.replace(/D1/g, 'Ab'); 
-	translated = translated.replace(new RegExp('D1', 'g'), 'Ab');
+	translated = translated.replace(/D1/gi, 'Ab'); 
+	translated = translated.replace(new RegExp('D1', 'gi'), 'Ab');
 
-	translated = translated.replace(/D2/g, 'A'); 
-	translated = translated.replace(new RegExp('D2', 'g'), 'A');
+	translated = translated.replace(/D2/gi, 'A'); 
+	translated = translated.replace(new RegExp('D2', 'gi'), 'A');
 
-	translated = translated.replace(/N1/g, 'Bb'); 
-	translated = translated.replace(new RegExp('N1', 'g'), 'Bb');
+	translated = translated.replace(/N1/gi, 'Bb'); 
+	translated = translated.replace(new RegExp('N1', 'gi'), 'Bb');
 
-	translated = translated.replace(/N2/g, 'B'); 
-	translated = translated.replace(new RegExp('N2', 'g'), 'B');
+	translated = translated.replace(/N2/gi, 'B'); 
+	translated = translated.replace(new RegExp('N2', 'gi'), 'B');
 
   }
 
